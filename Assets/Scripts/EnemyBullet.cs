88cf1c281daf7be06855884a94514b3ba8e723b6
Jeremia <jeremia_jason@yahoo.com>
2018-03-28 12:20:23 +0700

    add enemy shooter

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+
+	[HideInInspector]
+	public bool facingRight = false;
+
+	// Use this for initialization
+	void Start () {
+		if (facingRight == true) {
+			Flip ();
+		}
+	}
+
+	public void Flip ()
+	{
+		// Switch the way the enemy is labelled as facing.
+		facingRight = !facingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D col) 
+	{
+		if(col.gameObject.tag != "Enemy")
+		{
+			Destroy (gameObject);
+		}
+
+	}
+}
