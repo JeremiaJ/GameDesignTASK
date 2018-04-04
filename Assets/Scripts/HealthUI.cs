fef607e75a88736f6541c2eeca8d56beb59703a3
Gaudensius D P S <13514059@std.stei.itb.ac.id>
2018-04-04 13:24:48 +0700

    Added UI Asset and HealthUI Script

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour {
+	public GameObject Health1, Health2, Health3;
+	public int Health = 3;
+	private List<GameObject> Healths;
+	private bool TakenDamage;
+	private bool HealthRecovered;
+
+	void Awake () {
+		Healths = new List<GameObject>();
+		Healths.Add (Health1);
+		Healths.Add (Health2);
+		Healths.Add (Health3);
+		foreach (GameObject Health in Healths){
+			Health.SetActive (false);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject Health in Healths){
+			Health.SetActive (true);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (TakenDamage) {
+			TakenDamage = false;
+			if (Health > 0) {
+				Health -= 1;
+				Healths [Health].SetActive (false);
+			}
+		}
+		if (HealthRecovered) {
+			HealthRecovered = false;
+			if (Health < 3) {
+				Healths [Health].SetActive (true);
+				Health += 1;
+			}
+		}
+	}
+
+	public void TakeDamage () {
+		TakenDamage = true;
+	}
+
+	public void RecoverHealth () {
+		HealthRecovered = true;
+	}
+}
---
+++ Assets/Scripts/UITest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITest : MonoBehaviour {
+	public HealthUI CanvasUI;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.LeftBracket))
+			CanvasUI.TakeDamage ();
+		if (Input.GetKeyUp (KeyCode.RightBracket))
+			CanvasUI.RecoverHealth ();
+	}
+}
