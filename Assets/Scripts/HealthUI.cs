cdcbe9088fb1f8a31758f802db613ffa64f1dc36
Gaudensius D P S <13514059@std.stei.itb.ac.id>
2018-04-18 12:44:04 +0700

    Finished Inventory and gameplay UI

--- Assets/Scripts/HealthUI.cs
+++ Assets/Scripts/HealthUI.cs
@@ -4,52 +4,53 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour {
-	public GameObject Health1, Health2, Health3;
-	public int Health = 3;
-	private List<GameObject> Healths;
-	private bool TakenDamage;
-	private bool HealthRecovered;
-
-	void Awake () {
-		Healths = new List<GameObject>();
-		Healths.Add (Health1);
-		Healths.Add (Health2);
-		Healths.Add (Health3);
-		foreach (GameObject Health in Healths){
-			Health.SetActive (false);
-		}
-	}
+	public GameObject HealthInit;
+	public float HealthUIDistance;
+	public int MaxHealth = 3;
+	private int Health;
+	private List<GameObject> HealthsUI;
 
 	// Use this for initialization
 	void Start () {
-		foreach (GameObject Health in Healths){
-			Health.SetActive (true);
+		Health = MaxHealth;
+
+		HealthsUI = new List<GameObject> ();
+		HealthsUI.Add(HealthInit);
+		GameObject Temp;
+		Vector3 position = new Vector3 (50, 0, 0);
+		for (int i = 1; i < MaxHealth; i++) {
+			Temp = new GameObject ("Health");
+			Temp.AddComponent<RectTransform> ();
+			Temp.AddComponent<CanvasRenderer> ();
+			Temp.AddComponent<Image> ();
+			Temp.transform.SetParent(HealthInit.transform);
+			Temp.layer = 5;
+			Temp.transform.localPosition = position;
+			position.x += HealthUIDistance;
+			Temp.GetComponent<RectTransform>().sizeDelta = HealthInit.GetComponent<RectTransform>().sizeDelta;
+			Temp.GetComponent<Image> ().sprite = HealthInit.GetComponent<Image>().sprite;
+			HealthsUI.Add (Temp);
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (TakenDamage) {
-			TakenDamage = false;
-			if (Health > 0) {
-				Health -= 1;
-				Healths [Health].SetActive (false);
-			}
-		}
-		if (HealthRecovered) {
-			HealthRecovered = false;
-			if (Health < 3) {
-				Healths [Health].SetActive (true);
-				Health += 1;
-			}
-		}
+
 	}
 
 	public void TakeDamage () {
-		TakenDamage = true;
+		if (Health > 0) {
+			Health -= 1;
+			HealthsUI[Health].SetActive(false);
+//			LocalCanvas.HealthsUI [Health].SetActive (false);
+		}
 	}
 
 	public void RecoverHealth () {
-		HealthRecovered = true;
+		if (Health < MaxHealth) {
+			HealthsUI[Health].SetActive(true);
+//			LocalCanvas.HealthsUI [Health].SetActive (true);
+			Health += 1;
+		}
 	}
 }
--- Assets/Scripts/UITest.cs
+++ Assets/Scripts/UITest.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UITest : MonoBehaviour {
-	public HealthUI CanvasUI;
-
+	public GameObject HealthUI;
+	public Inventory Test;
 	// Use this for initialization
 	void Start () {
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyUp (KeyCode.LeftBracket))
-			CanvasUI.TakeDamage ();
+			HealthUI.GetComponent<HealthUI> ().TakeDamage ();
 		if (Input.GetKeyUp (KeyCode.RightBracket))
-			CanvasUI.RecoverHealth ();
+			HealthUI.GetComponent<HealthUI>().RecoverHealth ();
+		if (Input.GetKeyUp (KeyCode.Semicolon))
+			Test.SwitchLeft();
+		if (Input.GetKeyUp (KeyCode.Quote))
+			Test.SwitchRight();
+		if (Input.GetKeyUp (KeyCode.Period))
+			Test.UseAmmo(1);
+		if (Input.GetKeyUp (KeyCode.Slash))
+			Test.GetWeapon(1, 1);
 	}
 }
