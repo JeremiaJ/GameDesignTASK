924bcd0982dd8c1f4906662b543034702facbb17
Zixus <gaudensius.dimas@gmail.com>
2018-05-06 17:03:23 +0700

    Tweaked HealthUI for easier usage, edited CanvasUI prefab, added UITest scene for example purpose

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour {
+	public int MaxHealth = 3;
+	public int CurrentHealth;
+	[HideInInspector]
+	public bool HealthChanged; //to give signal to UI
+
+	// Use this for initialization
+	void Start () {
+		CurrentHealth = MaxHealth;
+		HealthChanged = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CurrentHealth <= 0)
+			; //Handle death here
+	}
+
+	public void TakeDamage (int damage) {
+		if (damage > 0){
+			if (CurrentHealth > 0) {
+				CurrentHealth -= damage;
+				if (CurrentHealth < 0)
+					CurrentHealth = 0;
+				HealthChanged = true;
+			}
+		}
+	}
+
+	public void RecoverHealth (int recovery) {
+		if (recovery > 0){
+			if (CurrentHealth < MaxHealth) {
+				CurrentHealth += recovery;
+				if (CurrentHealth > MaxHealth)
+					CurrentHealth = MaxHealth;
+				HealthChanged = true;
+			}
+		}
+	}
+}
