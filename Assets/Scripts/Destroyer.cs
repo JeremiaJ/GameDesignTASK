c7cc1dfdc9bcbf5b478301b7478d9ba7ce343372
Zixus <gaudensius.dimas@gmail.com>
2018-05-16 09:55:21 +0700

    Adding Zombies Ammos etc, Added destroy animation after explosion

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class Destroyer : MonoBehaviour
 {
-	public bool destroyOnAwake;			// Whether or not this gameobject should destroyed after a delay, on Awake.
-	public float awakeDestroyDelay;		// The delay for destroying it on Awake.
+	public bool destroyOnAwake = true;			// Whether or not this gameobject should destroyed after a delay, on Awake.
+	public float awakeDestroyDelay = 1f;		// The delay for destroying it on Awake.
 	public bool findChild = false;				// Find a child game object and delete it
 	public string namedChild;			// Name the child object in Inspector
 
